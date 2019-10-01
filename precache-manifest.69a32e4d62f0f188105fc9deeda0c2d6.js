62ea4c38d3f8a2050da6b3022f710e06f70f1bd4
Deanna <dt1543@nyu.edu>
2019-10-01 11:42:27 -0400

    Updates

---
+++ precache-manifest.69a32e4d62f0f188105fc9deeda0c2d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e04f6de941ba0bd57ffdd25c4ab1d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e49ed716f0fa44863533",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1a2fcf3585b3830017c3",
+    "url": "/static/js/2.799f07ba.chunk.js"
+  },
+  {
+    "revision": "e49ed716f0fa44863533",
+    "url": "/static/js/main.e4e3074e.chunk.js"
+  },
+  {
+    "revision": "aa17f785c7db94ffdaaf",
+    "url": "/static/js/runtime-main.008d490d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
