2dee474306a7d22e76c1c7eb084fe2ec45edac92
Deanna Tran <deanna@Deannas-MacBook-Pro.local>
2024-03-11 17:24:26 -0400

    change config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-// @ts-check
- 
-module.exports = async (phase, { defaultConfig }) => {
-    /**
-     * @type {import('next').NextConfig}
-     */
-    const nextConfig = {
-      /* config options here */
-    }
-    return nextConfig
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/my-website",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
